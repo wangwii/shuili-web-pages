6f25209288191886e7c125b2b2c27d6f436e16bb
yunhe.ww <yunhe.ww@alibaba-inc.com>
2020-06-28 19:55:54 +0800

    deploy new version

---
+++ precache-manifest.84afa049f107809e18010371b61885a0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af73ed25bf45859fd012",
+    "url": "/shuili-web-pages/css/app.e87e8226.css"
+  },
+  {
+    "revision": "d811c3ed8250647dfb0d",
+    "url": "/shuili-web-pages/css/chunk-vendors.12a0a5e3.css"
+  },
+  {
+    "revision": "00ef3e8f36c1885bddffcade72b6660c",
+    "url": "/shuili-web-pages/img/back.png"
+  },
+  {
+    "revision": "3b4df6eb4080f6b9e53cb31ae633970d",
+    "url": "/shuili-web-pages/img/heliu3.3b4df6eb.jpg"
+  },
+  {
+    "revision": "8cd431a2649332080d1f5ccc80b69278",
+    "url": "/shuili-web-pages/img/heliu4.8cd431a2.jpg"
+  },
+  {
+    "revision": "3ca0c723e3087deaf34467a249d60d3a",
+    "url": "/shuili-web-pages/img/heliu5.3ca0c723.jpg"
+  },
+  {
+    "revision": "2109417cffd4820aed0062ca6ff3a5ae",
+    "url": "/shuili-web-pages/img/touxiang1.2109417c.jpg"
+  },
+  {
+    "revision": "76eb5a21d14a6056333ea780ea5742bd",
+    "url": "/shuili-web-pages/img/touxiang2.76eb5a21.jpg"
+  },
+  {
+    "revision": "42d7e324f59cb1ec886bf71dea176e14",
+    "url": "/shuili-web-pages/img/touxiang3.42d7e324.jpg"
+  },
+  {
+    "revision": "98409637af06590d1a919c8247c04be3",
+    "url": "/shuili-web-pages/index.html"
+  },
+  {
+    "revision": "af73ed25bf45859fd012",
+    "url": "/shuili-web-pages/js/app.6f48911a.js"
+  },
+  {
+    "revision": "2eb6f8aea534ffd45f6b",
+    "url": "/shuili-web-pages/js/chunk-2d217357.11d3ff3d.js"
+  },
+  {
+    "revision": "d811c3ed8250647dfb0d",
+    "url": "/shuili-web-pages/js/chunk-vendors.cccac00a.js"
+  },
+  {
+    "revision": "9a8cdd84feb58ca775745cacf03712c9",
+    "url": "/shuili-web-pages/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shuili-web-pages/robots.txt"
+  }
+]);
---
+++ precache-manifest.934957bce15df86fe8ca67b06413a590.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f584def009b42d161f37",
+    "url": "/shuili-web-pages/css/app.e87e8226.css"
+  },
+  {
+    "revision": "fd7603040b93fd45334d",
+    "url": "/shuili-web-pages/css/chunk-vendors.12a0a5e3.css"
+  },
+  {
+    "revision": "3b4df6eb4080f6b9e53cb31ae633970d",
+    "url": "/shuili-web-pages/img/heliu3.3b4df6eb.jpg"
+  },
+  {
+    "revision": "8cd431a2649332080d1f5ccc80b69278",
+    "url": "/shuili-web-pages/img/heliu4.8cd431a2.jpg"
+  },
+  {
+    "revision": "3ca0c723e3087deaf34467a249d60d3a",
+    "url": "/shuili-web-pages/img/heliu5.3ca0c723.jpg"
+  },
+  {
+    "revision": "2109417cffd4820aed0062ca6ff3a5ae",
+    "url": "/shuili-web-pages/img/touxiang1.2109417c.jpg"
+  },
+  {
+    "revision": "76eb5a21d14a6056333ea780ea5742bd",
+    "url": "/shuili-web-pages/img/touxiang2.76eb5a21.jpg"
+  },
+  {
+    "revision": "42d7e324f59cb1ec886bf71dea176e14",
+    "url": "/shuili-web-pages/img/touxiang3.42d7e324.jpg"
+  },
+  {
+    "revision": "2f6aaa652a049c60dce121ea8179d57b",
+    "url": "/shuili-web-pages/index.html"
+  },
+  {
+    "revision": "f584def009b42d161f37",
+    "url": "/shuili-web-pages/js/app-legacy.11272067.js"
+  },
+  {
+    "revision": "9165c141ffaa704b6c98",
+    "url": "/shuili-web-pages/js/chunk-2d217357-legacy.c163eebc.js"
+  },
+  {
+    "revision": "fd7603040b93fd45334d",
+    "url": "/shuili-web-pages/js/chunk-vendors-legacy.defca2ca.js"
+  },
+  {
+    "revision": "9a8cdd84feb58ca775745cacf03712c9",
+    "url": "/shuili-web-pages/manifest.json"
+  }
+]);
